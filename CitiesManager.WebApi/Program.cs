7ef03461d7bbbdfb45a0bf11376ea4a5f6aee585
C4r50nC <xichen059@gmail.com>
2024-12-22 16:25:24 +1100

    feat: add Swagger / Open API

--- CitiesManager.WebApi/Program.cs
+++ CitiesManager.WebApi/Program.cs
@@ -12,12 +12,19 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
 });
 
+// Swagger services
+builder.Services.AddEndpointsApiExplorer(); // Generates description for all endpoints
+builder.Services.AddSwaggerGen(); // Generates Open API specification
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
 app.UseHsts();
 app.UseHttpsRedirection();
+
+app.UseSwagger(); // Creates endpoints for swagger.json
+app.UseSwaggerUI(); // Creates Swagger UI for testing API endpoints
 
 app.UseAuthorization();
 
