5e0816a9a6a367e3d69797cf0047b3c475455d37
C4r50nC <xichen059@gmail.com>
2024-12-25 18:01:20 +1100

    feat: add content negotiation to restrict produced and consumed data type

--- CitiesManager.WebApi/Controllers/CitiesController.cs
+++ CitiesManager.WebApi/Controllers/CitiesController.cs
@@ -27,6 +27,7 @@ namespace CitiesManager.WebApi.Controllers
         /// </summary>
         /// <returns>List of Cities</returns>
         [HttpGet]
+        [Produces("application/xml")]
         public async Task<ActionResult<IEnumerable<City>>> GetCities()
         {
             return await _context.Cities.OrderBy(city => city.CityName).ToListAsync();
