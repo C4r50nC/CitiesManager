e31dc4ce71c685f460f84b54696ced840e5746fc
C4r50nC <xichen059@gmail.com>
2024-12-28 12:26:58 +1100

    feat: add link to cities component on home page

--- CitiesManager.WebApi/ClientApp/CitiesAngularApp/src/app/app.component.ts
+++ CitiesManager.WebApi/ClientApp/CitiesAngularApp/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [RouterOutlet],
+  imports: [RouterModule, RouterOutlet],
 })
 export class AppComponent {}
